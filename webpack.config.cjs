521fabee20f6098e5c2c2c31671b85faa10e91a5
Luke Tchang <ltchang@stanford.edu>
2023-01-06 17:12:35 -0700

    replace rollup with webpack for filling buffer

---
+++ webpack.config.cjs
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    filename: "main.cjs",
+    path: path.resolve(__dirname, "build"),
+  },
+  resolve: {
+    fallback: {
+      buffer: require.resolve("buffer/"),
+      crypto: require.resolve("crypto-browserify"),
+      stream: require.resolve("stream-browserify"),
+      os: require.resolve("os-browserify"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      Buffer: ["buffer", "Buffer"],
+    }),
+    new webpack.ProvidePlugin({
+      crypto: "crypto-browserify",
+    }),
+  ],
+};

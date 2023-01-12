d961943afee42b5ee1761cb6da25d4c70eb24476
Sladuca <sladuca777@gmail.com>
2023-01-12 17:28:31 -0500

    update rollup config

--- rollup.cjs.config.js
+++ rollup.cjs.config.js
@@ -1,18 +1,22 @@
 import fs from "fs";
 import { builtinModules as builtin } from "module";
 import nodePolyfills from "rollup-plugin-polyfill-node";
+import resolve from "@rollup/plugin-node-resolve";
+
 
 const pkg = JSON.parse(fs.readFileSync("./package.json"));
 
 export default {
-    input: "main.js",
-    output: {
-        file: "build/main.cjs",
-        format: "cjs",
-    },
-    external: [
-        ...Object.keys(pkg.dependencies),
-        ...builtin,
-    ],
-    plugins: [ nodePolyfills({ include: null }) ],
+  input: "main.js",
+  output: {
+    file: "build/main.cjs",
+    format: "cjs",
+  },
+  external: [...Object.keys(pkg.dependencies), ...builtin],
+  plugins: [
+    nodePolyfills({
+      include: ["./node_modules/**/*.js", "./src/*.js"],
+    }),
+    resolve(),
+  ],
 };
